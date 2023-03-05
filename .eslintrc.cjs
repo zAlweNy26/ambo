5e9663091f76e5286128ada8f0a29edc648157b7
Dany <zdanymc@gmail.com>
2023-03-05 21:37:38 +0100

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   extends: [
-  "plugin:astro/recommended",
+    "plugin:astro/recommended",
     'eslint:recommended',
     'plugin:vue/vue3-recommended',
     "plugin:@typescript-eslint/recommended",
