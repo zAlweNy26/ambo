350da35e616e61a109e44152936b1cc022851d67
Dany <zdanymc@gmail.com>
2023-10-16 01:12:04 +0200

    fixed icons

--- src/components/RegisterPWA.vue
+++ src/components/RegisterPWA.vue
@@ -7,24 +7,20 @@ function close() {
     offlineReady.value = false
     needRefresh.value = false
 }
-
-async function updateSW() {
-    await updateServiceWorker()
-}
 </script>
 
 <template>
     <div v-if="offlineReady || needRefresh" 
         class="flex flex-wrap bg-primary items-center justify-center p-2 text-base-100 font-medium gap-4" role="alert">
-        <div class="text-center">
+        <div class="text-center text-sm">
             <span v-if="offlineReady">App pronta per funzionare offline</span>
             <span v-else>L'app può essere aggiornata, per favore clicca 'Aggiorna'.</span>
         </div>
         <div class="flex gap-2">
-            <button v-if="needRefresh" @click="updateSW()" class="btn btn-xs btn-success">
+            <button v-if="needRefresh" @click="updateServiceWorker()" class="btn btn-sm btn-success">
                 Aggiorna
             </button>
-            <button @click="close()" class="btn btn-xs btn-error">
+            <button @click="close()" class="btn btn-sm btn-error">
                 Chiudi
             </button>
         </div>
